909b03bc0bb829389a1b518ba6ff3e3082900a82
taro-ball <22948157+taro-ball@users.noreply.github.com>
2023-03-03 20:58:46 +1300

    fix

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port  = 80
   to_port    = 80
   protocol   = "tcp"
-  cidr_block = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id=aws_security_group.blog.id
 }
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port  = 443
   to_port    = 443
   protocol   = "tcp"
-  cidr_block = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id=aws_security_group.blog.id
 }
@@ -66,7 +66,7 @@ resource "aws_security_group_rule" "blog_everythn_out" {
   from_port  = 80
   to_port    = 80
   protocol   = "tcp"
-  cidr_block = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id=aws_security_group.blog.id
 }
