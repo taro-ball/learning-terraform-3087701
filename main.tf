666fdbe66ec86f10a01ffa2485f8a32bc52fc9d4
taro-ball <22948157+taro-ball@users.noreply.github.com>
2023-03-06 22:52:20 +1300

    add alb

--- main.tf
+++ main.tf
@@ -65,3 +65,43 @@ resource "aws_instance" "web_vlg" {
   }
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.aws_vpc.vpc_id
+  subnets            = module.aws_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.web_vlg.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
