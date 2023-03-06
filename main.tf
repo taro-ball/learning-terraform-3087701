f6ad0d7683a2ca36c5bd965a017553fc936410b8
taro-ball <22948157+taro-ball@users.noreply.github.com>
2023-03-06 22:54:08 +1300

    fix SG array

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "alb" {
 
   vpc_id             = module.aws_vpc.vpc_id
   subnets            = module.aws_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = [module.blog_sg.security_group_id]
 
 
   target_groups = [
