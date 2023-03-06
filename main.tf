7b49596afd3cc186955dbb6561c7496818527759
taro-ball <22948157+taro-ball@users.noreply.github.com>
2023-03-06 22:42:58 +1300

    fix vpc

--- main.tf
+++ main.tf
@@ -39,6 +39,19 @@ module "aws_vpc" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+  name    = "blog_new"
+
+  vpc_id              = module.aws_vpc.vpc_id
+  
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
 
 resource "aws_instance" "web_vlg" {
   ami           = data.aws_ami.app_ami.id
@@ -52,15 +65,3 @@ resource "aws_instance" "web_vlg" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
-  name    = "blog_new"
-
-  vpc_id              = module.aws_vpc.public_subnets[0]
-  
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
