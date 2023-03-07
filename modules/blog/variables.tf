47e951d7094718dbb3b99d4250d4f041dc0903dc
taro-ball <22948157+taro-ball@users.noreply.github.com>
2023-03-08 12:50:33 +1300

    move aws vars

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -1,3 +1,11 @@
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
