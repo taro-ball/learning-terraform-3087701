18b3bd551895940bfb2cb336a2c9b1891b762781
taro-ball <22948157+taro-ball@users.noreply.github.com>
2024-01-11 21:25:36 +0000

    Revert "sec group add"
    This reverts commit d4ce559f7cb18d657c6fcfbd0096b3f183f6f959.

--- main.tf
+++ main.tf
@@ -22,51 +22,11 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{
-  default=true
-}
-
 resource "aws_instance" "web_vlg" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
-  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
-}
-
-resource "aws_security_group" "blog"{
-  name        = "blog"
-  description = "Allow http(s) in. everything out"
-
-  vpc_id      = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type= "ingress"
-  from_port  = 80
-  to_port    = 80
-  protocol   = "tcp"
-  cidr_block = ["0.0.0.0/0"]
-
-  security_group_id=aws_security_group.blog.id
-}
-resource "aws_security_group_rule" "blog_https_in" {
-  type= "ingress"
-  from_port  = 443
-  to_port    = 443
-  protocol   = "tcp"
-  cidr_block = ["0.0.0.0/0"]
-
-  security_group_id=aws_security_group.blog.id
-}
-resource "aws_security_group_rule" "blog_everythn_out" {
-  type= "egress"
-  from_port  = 80
-  to_port    = 80
-  protocol   = "tcp"
-  cidr_block = ["0.0.0.0/0"]
-
-  security_group_id=aws_security_group.blog.id
 }
