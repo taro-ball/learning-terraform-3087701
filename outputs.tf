3a500ba9822b8a90bf65be5a5f0e1efd578c305f
taro-ball <22948157+taro-ball@users.noreply.github.com>
2024-01-11 21:25:37 +0000

    Revert "fix nme"
    This reverts commit 137f8a8ce3031c98c09d1b5855a39bd85f29d92d.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.web_vlg.ami
+  value = aws_instance.web.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.web_vlg.arn
+  value = aws_instance.web.arn
 }
