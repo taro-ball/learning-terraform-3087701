1398bcf96633e72ff73820c5ee122e5c4f5a3655
taro-ball <22948157+taro-ball@users.noreply.github.com>
2024-01-11 21:25:32 +0000

    Revert "cleanup"
    This reverts commit 458271e4de7e5be4fcf886f6f9a62a1fb2e12f68.

--- main.tf
+++ main.tf
@@ -49,3 +49,38 @@ module "blog_sg" {
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group" "blog"{
+  name        = "blog"
+  description = "Allow http(s) in. everything out"
+
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type= "ingress"
+  from_port  = 80
+  to_port    = 80
+  protocol   = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id=aws_security_group.blog.id
+}
+resource "aws_security_group_rule" "blog_https_in" {
+  type= "ingress"
+  from_port  = 443
+  to_port    = 443
+  protocol   = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id=aws_security_group.blog.id
+}
+resource "aws_security_group_rule" "blog_everythn_out" {
+  type= "egress"
+  from_port  = 80
+  to_port    = 80
+  protocol   = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id=aws_security_group.blog.id
+}
