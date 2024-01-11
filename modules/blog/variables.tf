52ff572572ab4c463b32dd8722bab43d09c51a19
taro-ball <22948157+taro-ball@users.noreply.github.com>
2024-01-11 21:24:46 +0000

    Revert "no aws vars"
    This reverts commit bd78b4fef905daa86a638c87307e69d620d4d683.

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -1,10 +1,10 @@
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   default = ""
-# }
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+}
 
-# variable "AWS_ACCESS_KEY_ID" {
-#   default = ""
-# }
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+}
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
