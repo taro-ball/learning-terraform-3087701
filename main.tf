b9f2524137f1ef3da71e383bb08e470684669a24
Taro B <taro@empyre.tst>
2024-01-15 11:09:47 -0500

    typo

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "allow http/s, everything out"
-  vpc_id      = data.aws_vpc.default
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
