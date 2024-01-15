0d9d2f06be1e4df3f9daa95f825a7df442f99501
Taro B <taro@empyre.tst>
2024-01-15 15:49:24 -0500

    cleanup sg resource

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
+  name    = "blog"
   # all-all 
 
   ingress_rules       = ["http-80-tcp", "https-8443-tcp"]
@@ -41,40 +41,4 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
   vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "allow http/s, everything out"
-  vpc_id      = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_out" {
-  type        = "ingress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
